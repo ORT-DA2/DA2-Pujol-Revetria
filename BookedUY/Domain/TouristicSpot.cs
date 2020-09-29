48c1e1ce18b0bf22f2672ddb9be6e360ec8d3c58
Sera Revetria <serafinrevetria@gmail.com>
2020-09-29 09:43:22 -0300

    TDD TouristicSpot

--- BookedUY/Domain/TouristicSpot.cs
+++ BookedUY/Domain/TouristicSpot.cs
@@ -2,16 +2,59 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace Domain
 {
     public class TouristicSpot
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new NullInputException();
+                }
+                else
+                {
+                    _name = value.Trim();
+                }
+            }
+        }
         public Region Region { get; set; }
         public int RegionId { get; set; }
-        public string Description { get; set; }
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new NullInputException();
+                }
+                else
+                {
+                    _description = value.Trim();
+                }
+            }
+        }
         public List<CategoryTouristicSpot> Categories { get; set; }
         public List<Accommodation> Accommodations { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            var result = false;
+            if (obj is TouristicSpot touristicSpot)
+            {
+                result = this.Id == touristicSpot.Id && this.Name == touristicSpot.Name;
+            }
+            return result;
+        }
     }
 }
+
