b3bb453bf5cf55929dceeec1e0df3452e4d28a92
Sera Revetria <serafinrevetria@gmail.com>
2020-09-30 18:41:42 -0300

    BookingStageRepo Integration Test

---
+++ BookedUY/DataAccess.Tests/BookingStageRepositoryTest.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Context;
+using DataAccess.Repositories;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Tests
+{
+    [TestClass]
+    public class BookingStageRepositoryTest
+    {
+        [TestMethod]
+        public void TestGetAllRegionsOk()
+        {
+            List<BookingStage> bookingStagesToReturn = new List<BookingStage>()
+            {
+                new BookingStage()
+                {
+                    Id=1,
+                    Description="The booking was added correctly, awaiting payment",
+                    Administrator = null,
+                    AdministratorId=0,
+                    AsociatedBooking=null,
+                    AsociatedBookingId=0,
+                    EntryDate= DateTime.Now,
+                    Status = new Status(),
+                },
+                new BookingStage()
+                {
+                    Id=2,
+                    Description = "The booking was rejected due to payment issues, please contact your bank",
+                    Administrator = null,
+                    AdministratorId = 0,
+                    AsociatedBooking = null,
+                    AsociatedBookingId = 0,
+                    EntryDate = DateTime.Now,
+                    Status = new Status(),
+                },
+            };
+            var options = new DbContextOptionsBuilder<BookedUYContext>()
+                .UseInMemoryDatabase(databaseName: "BookedUYDB").Options;
+            var context = new BookedUYContext(options);
+            bookingStagesToReturn.ForEach(r => context.Add(r));
+            context.SaveChanges();
+            var repository = new BookingStageRepository(context);
+
+            var result = repository.GetAll();
+
+            Assert.IsTrue(bookingStagesToReturn.SequenceEqual(result));
+        }
+    }
+}
--- BookedUY/DataAccess/Repositories/BookingStageRepository.cs
+++ BookedUY/DataAccess/Repositories/BookingStageRepository.cs
@@ -1,4 +1,6 @@
 ﻿using DataAccessInterface;
+using Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +9,18 @@ namespace DataAccess.Repositories
 {
     public class BookingStageRepository : IBookingStageRepository
     {
+        private readonly DbSet<BookingStage> bookingStages;
+        private readonly DbContext bookUYContext;
+
+        public BookingStageRepository(DbContext bookUYContext)
+        {
+            this.bookUYContext = bookUYContext;
+            this.bookingStages = bookUYContext.Set<BookingStage>();
+        }
+
+        public IEnumerable<BookingStage> GetAll()
+        {
+            return this.bookingStages;
+        }
     }
 }
