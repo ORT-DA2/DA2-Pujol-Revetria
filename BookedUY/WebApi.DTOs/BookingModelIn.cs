1f33d5db03942e68463ecdebd2f669c4de9c4599
Facundo <facundo.pujol@2innovateit.com>
2020-10-10 11:27:58 -0300

    New DTOs

--- BookedUY/WebApi.DTOs/BookingModelIn.cs
+++ BookedUY/WebApi.DTOs/BookingModelIn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,6 +8,11 @@ namespace WebApi.DTOs
     public class BookingModelIn
     {
         public int AccommodationId { get; set; }
-        
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public string GuestName { get; set; }
+        public string GuestEmail { get; set; }
+        public string GuestLastName { get; set; }
+        public Guest[] Guests { get; set; }
     }
 }
--- BookedUY/WebApi.DTOs/BookingModelOut.cs
+++ BookedUY/WebApi.DTOs/BookingModelOut.cs
@@ -7,6 +7,14 @@ namespace WebApi.DTOs
     public class BookingModelOut
     {
         public int Id { get; set; }
+        public int AccommodationId { get; set; }
+        public string AccommodationName { get; set; }
+        public string AccommodationAddress { get; set; }
+        public string AccommodationContact { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public double Price { get; set; }
+        public string GuestEmail { get; set; }
 
     }
 }
